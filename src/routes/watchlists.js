af8b79c1c661bf03a8c9e4d5ccd54511439b822a
LJ5O <75009579+LJ5O@users.noreply.github.com>
2023-02-03 10:58:14 +0100

    Film state can now be updated
    Must be in ["Watch later", "Watching", "Finished", "Canceled"]

--- src/routes/watchlists.js
+++ src/routes/watchlists.js
@@ -102,7 +102,61 @@ module.exports = async(app)=>{
       return;
     }
 
-    watchlist.movies.push(req.body.movieId);
+    watchlist.movies.push({"id":req.body.movieId, "state":"Watch later"});
+
+    watchlistsControllers.setMovies(watchlist)
+    .then(()=>{
+      return res.json(watchlist);
+    });
+
+  });
+
+  app.post('/watchlists/setmoviestate', async(req, res)=>{
+    if(req.body.watchlistId==undefined || req.body.movieId==undefined || req.body.movieState==undefined){
+      res.status(400).json({
+        "route": "/watchlists/setmoviestate",
+        "args": {
+          "watchlistId": "A String representing the ID of the watchlist",
+          "movieId": "The ID of the film to add to this watchlist",
+          "movieState": "\"Watch later\", \"Watching\", \"Finished\" or \"Canceled\"."
+        },
+        "return": "The edited waitchlist object"
+      });
+      return;
+    }
+
+    let watchlist = await watchlistsControllers.findWatchlistWithID(req.body.watchlistId)
+    if(! watchlist){
+      res.status(400).json({
+        "error": "Watchlist don't exists !"
+      });
+      return;
+    }
+
+    if(! ["Watch later", "Watching", "Finished", "Canceled"].includes(req.body.movieState)){
+      res.status(400).json({
+        "error": "Invalid movie state !"
+      });
+      return;
+    }
+
+    somethingEdited = false;//Will allow us to detect movies that don't belong to this watchlist
+    for(let i=0; i<watchlist.movies.length; i++){//We will find the movie that must be edited
+        if(watchlist.movies[i].id==req.body.movieId){
+          //This is the movie that musy be edited
+          watchlist.movies[i].state = req.body.movieState;
+          somethingEdited = true;
+          break;
+        }
+    }
+
+    if(!somethingEdited){
+      res.status(400).json({
+        "error": "Movie don't exists !"
+      });
+      return;
+    }
+
 
     watchlistsControllers.setMovies(watchlist)
     .then(()=>{
